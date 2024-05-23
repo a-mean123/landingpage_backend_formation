2a5996d85d028a690753dd375b9b7df175b84ff6
a-mean123 <mohamedamine.jebali@esen.tn>
2024-05-23 18:48:29 +0100

    remove mailing

--- controllers/newsletter.controller.js
+++ controllers/newsletter.controller.js
@@ -7,17 +7,8 @@ const create = async (req, res) => {
     try {
         let news = new Newsletter(req.body);
         let result = await news.save();
-        let emailObject = {
-            destination: news.email,
-            subject: 'subscription notification',
-            content: `<h1>New Subscription by : ${news.email}</h1>`
-        }
-        notifyAdmin(emailObject, res);
-
-        emailObject.destination = news.email;
-        emailObject.subject = 'Subscription notification';
-        emailObject.content = `<h1>Your subscription has been sent</h1>`;
-        notifyUser(emailObject, res);
+    
+  
 
         res.status(200).send(result);
 
